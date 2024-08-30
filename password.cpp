5081ba7acd3976005c70dcee115cdeb93d887eb0
ARYAN THORAT <bussinesswitharyanthorat@gmail.com>
2024-08-31 01:28:30 +0530

    Add files via upload

---
+++ password.cpp
@@ -0,0 +1,16 @@
+//Aryan thorat entc a2 032 6g password
+#include <iostream>
+using namespace std;
+int main() {
+int a,t;
+a=17062005;
+cout<<"Enter your pin";
+cin>>t;
+while(a!=t){
+cout<<"enter password ";
+cin>>t;
+}
+cout<<"login success";
+}
+/*Enter your pin 1706200517062005
+login success */
